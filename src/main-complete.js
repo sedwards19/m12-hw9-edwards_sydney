1df3bdcde55bad2f54157700fbc8a033f808a3e2
Sydney Edwards <sydneyedwards@Sydneys-MacBook-Air.local>
2018-11-12 00:45:28 -0500

    Add pd project files

---
+++ src/main-complete.js
@@ -0,0 +1,17 @@
+const navButtons = document.querySelectorAll('#navbar ul li');
+
+function wrapper() {
+  const loopThrough = () => {
+
+
+    for (let i = 0; i < thisButtonLength; i++) {
+
+    }
+
+  };
+  loopThrough();
+};
+
+for(let i = 0; i < navButtons.length; i++) {
+
+};
