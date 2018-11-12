5817cd00aee11f591f306582c1e887d16401cdf7
Sydney Edwards <sydneyedwards@Sydneys-MacBook-Air.local>
2018-11-12 01:19:24 -0500

    Change nav to toggle red

--- src/main-complete.js
+++ src/main-complete.js
@@ -1,11 +1,17 @@
 const navButtons = document.querySelectorAll('#navbar ul li');
+const navBar = document.querySelector('#navbar');
 
 function wrapper() {
+
+  const changeColor = () => {
+  	navbar.classList.toggle('red');
+  };
+
   const loopThrough = () => {
 
 
     for (let i = 0; i < thisButtonLength; i++) {
-
+    	changeColor();
     }
 
   };
